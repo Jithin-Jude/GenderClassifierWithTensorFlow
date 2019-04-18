1071b6fad24d9e0c8ad3077b7b0836efa2bdf097
Jithin Jude <jithin.jude68@gmail.com>
2019-04-18 10:32:00 +0530

    Initial commit

---
+++ app/src/main/java/com/mountzoft/tflite/MainActivity.java
@@ -0,0 +1,205 @@
+package com.mountzoft.tflite;
+
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.wonderkiln.camerakit.CameraKitError;
+import com.wonderkiln.camerakit.CameraKitEvent;
+import com.wonderkiln.camerakit.CameraKitEventListener;
+import com.wonderkiln.camerakit.CameraKitImage;
+import com.wonderkiln.camerakit.CameraKitVideo;
+import com.wonderkiln.camerakit.CameraView;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String MODEL_PATH = "optimized_graph.lite";
+    private static final String LABEL_PATH = "retrained_labels.txt";
+    private static final int INPUT_SIZE = 299;
+
+    private Classifier classifier;
+
+    private Executor executor = Executors.newSingleThreadExecutor();
+    private TextView textViewResult;
+    private TextView textViewInfo;
+    private Button btnDetectObject;
+    private ImageView imageViewResult;
+    private CameraView cameraView;
+
+    MenuItem toggleCameraBtn;
+
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cameraView = findViewById(R.id.cameraView);
+        imageViewResult = findViewById(R.id.imageViewResult);
+        textViewResult = findViewById(R.id.textViewResult);
+        textViewInfo = findViewById(R.id.info);
+        textViewResult.setMovementMethod(new ScrollingMovementMethod());
+
+        textViewInfo.setText("Try to get a clear picture of face then tap Predict Gender button");
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Please wait");
+        progressDialog.setCancelable(false);
+
+        //btnToggleCamera = findViewById(R.id.btnToggleCamera);
+        btnDetectObject = findViewById(R.id.btnDetectObject);
+
+        cameraView.addCameraKitListener(new CameraKitEventListener() {
+            @Override
+            public void onEvent(CameraKitEvent cameraKitEvent) {
+
+            }
+
+            @Override
+            public void onError(CameraKitError cameraKitError) {
+
+            }
+
+            @Override
+            public void onImage(CameraKitImage cameraKitImage) {
+
+                Bitmap bitmap = cameraKitImage.getBitmap();
+
+                bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
+
+                btnDetectObject.setVisibility(View.GONE);
+                cameraView.setVisibility(View.GONE);
+                toggleCameraBtn.setVisible(false);
+                imageViewResult.setVisibility(View.VISIBLE);
+                textViewResult.setVisibility(View.VISIBLE);
+
+
+                imageViewResult.setImageBitmap(bitmap);
+
+                final List<Classifier.Recognition> results = classifier.recognizeImage(bitmap);
+
+                String resultsString = results.toString();
+                resultsString = resultsString.replace("[","");
+                resultsString = resultsString.replace("]","");
+                resultsString = resultsString.replace(",","\n");
+
+                Log.d("JITHIN DEBUG", resultsString);
+
+                textViewInfo.setText("Here is the result with confidence score. Tap on image for next prediction.");
+                progressDialog.dismiss();
+
+                if(resultsString.substring(0, 1).equals("f")){
+                    textViewResult.setText("Female\n"+resultsString.substring(8,25));
+                }else {
+                    textViewResult.setText("Male\n"+resultsString.substring(6,23));
+                }
+
+            }
+
+            @Override
+            public void onVideo(CameraKitVideo cameraKitVideo) {
+
+            }
+        });
+
+        btnDetectObject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressDialog.show();
+                cameraView.captureImage();
+            }
+        });
+
+        initTensorFlowAndLoadModel();
+    }
+
+    public void showCamera(View view){
+        imageViewResult.setVisibility(View.GONE);
+        textViewResult.setVisibility(View.GONE);
+        cameraView.setVisibility(View.VISIBLE);
+        btnDetectObject.setVisibility(View.VISIBLE);
+        toggleCameraBtn.setVisible(true);
+        textViewInfo.setText("Try to get a clear picture of face then tap Predict Gender button");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar_menu, menu);
+        toggleCameraBtn = menu.findItem(R.id.toggleCameraBtn);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.toggleCameraBtn) {
+            cameraView.toggleFacing();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraView.start();
+    }
+
+    @Override
+    protected void onPause() {
+        cameraView.stop();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                classifier.close();
+            }
+        });
+    }
+
+    private void initTensorFlowAndLoadModel() {
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    classifier = TensorFlowImageClassifier.create(
+                            getAssets(),
+                            MODEL_PATH,
+                            LABEL_PATH,
+                            INPUT_SIZE);
+                    makeButtonVisible();
+                } catch (final Exception e) {
+                    throw new RuntimeException("Error initializing TensorFlow!", e);
+                }
+            }
+        });
+    }
+
+    private void makeButtonVisible() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                btnDetectObject.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
